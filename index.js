f982ea9ce39f180de925ded9da31ffdb6e61fd18
Fabiano S <fabianosanttana@hotmail.com>
2018-11-26 21:56:25 -0200

    Commitkn

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ loginApi(app, firebase);
 usuarioApi(app, firebase);
 ticketsApi(app,firebase);
 
-app.listen(process.env.PORT, "0.0.0.0", () => {
-})
+app.listen(process.env.PORT, "0.0.0.0", () => {
+    console.log("Servidor rodando na porta" + process.env.PORT)
+}) 
